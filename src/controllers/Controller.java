d0285c28763dfef7388b5abf60845c28434cc843
Jaret Wright <hkkA223>
2021-02-10 13:48:53 -0500

    Added the shuffle of the deck and initialize method

--- src/controllers/Controller.java
+++ src/controllers/Controller.java
@@ -2,11 +2,15 @@ package controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import models.Card;
 import models.DeckOfCards;
 
-public class Controller {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
 
     @FXML
     private Label faceNameLabel;
@@ -17,15 +21,25 @@ public class Controller {
     @FXML
     private Label valueLabel;
 
-    private DeckOfCards deck = new DeckOfCards();
+    private DeckOfCards deck;
 
     @FXML
-    void dealTopCard(ActionEvent event) {
+    void dealTopCard() {
         Card card = deck.dealTopCard();
         faceNameLabel.setText(card.getFaceName());
         suitLabel.setText(card.getSuit());
         valueLabel.setText(Integer.toString(card.getFaceValue()));
     }
 
-
+    /**
+     * This method will be automatically called when the Scene is loaded
+     * @param url
+     * @param resourceBundle
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        deck = new DeckOfCards();
+        deck.shuffle();
+        dealTopCard();
+    }
 }
