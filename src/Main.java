51e34ad9cca66c87c7a855f6ab0d31433e362ce7
Jaret Wright <hkkA223>
2021-03-03 11:51:27 -0500

    Added new Controller and View files

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    /**
+     * This method was abstract in the Application class.  This
+     * is where we define what we want the application to do when
+     * it starts.
+     *
+     *In our case, we want our application to load the fxml file called
+     * cardView.fxml
+     */
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("views/cardView.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
